e3d719feac25ed1d219ae48d39e8d65f0eb03fde
Yahoosd <92000047+Yahoosd@users.noreply.github.com>
2024-10-15 10:21:52 +0800

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,16 @@ import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+        dispatch({ type: 'ADD_ITEM', item: product });
+        setAddedToCart((prevState) => ({ ...prevState, [product.name]: true }));
+      };
+    const ProductList = ({ dispatch, cart }) => {
+        const totalQuantity = Object.values(cart).reduce((acc, quantity) => acc + quantity, 0);
+      
+        // ...
+      };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -268,8 +277,21 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
-
+            {plantsArray.map((category, index) => (
+            <div key={index}>
+                <h1><div>{category.category}</div></h1>
+                <div className="product-list">
+                    {category.plants.map((plant, plantIndex) => (
+                    <div className="product-card" key={plantIndex}>
+                        <img className="product-image" src={plant.image} alt={plant.name} />
+                        <div className="product-title">{plant.name}</div>
+                        {/*Similarly like the above plant.name show other details like description and cost*/}
+                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                    </div>
+                    ))}
+                </div>
+            </div>
+            ))}
         </div>
  ) :  (
     <CartItem onContinueShopping={handleContinueShopping}/>
