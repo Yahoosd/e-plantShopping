582c3a112159cf3d9829df7ebc61f4fda93cd089
Yahoosd <92000047+Yahoosd@users.noreply.github.com>
2024-10-15 10:21:11 +0800

    CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -2,36 +2,52 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
-
+const store = configureStore({
+    reducer: {
+      cart: cartReducer
+    }
+  });
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => total + item.cost * item.quantity, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+      setTotalQuantity(totalQuantity - 1);
+    } else {
+      dispatch(removeItem(item.name));
+      setTotalQuantity(totalQuantity - item.quantity);
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+    setTotalQuantity(totalQuantity - item.quantity);
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item.quantity;
   };
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
